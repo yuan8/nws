95daaafe0bbaef1f614db8a9ff901329c157a386
yuan8 <bhalayuan@gmail.com>
2020-01-13 10:05:22 +0700

    ok

---
+++ application/controllers/pdam.php
@@ -0,0 +1,11 @@
+<?php
+defined('BASEPATH') OR exit('No direct script access allowed');
+
+class Pdam extends CI_Controller {
+
+  public function index(){
+
+    return view('pages.pdam');
+    
+  }
+}
